8ea3cc9b18fd7464639ba42fcbb98d1e5679a0b2
uisnettop <84020505+uisnettop@users.noreply.github.com>
2021-11-01 12:35:16 +0100

    init
    init

---
+++ precache-manifest.2cf8c55786514def9b53dd7e8382dc1d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d155ac2b36e289e23021",
+    "url": "css/app.26932dd1.css"
+  },
+  {
+    "revision": "a7a90ac83e17da969886",
+    "url": "css/xabout.b9db4020.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8e1dd5a995cde80ede615b9cd40f5dad",
+    "url": "index.html"
+  },
+  {
+    "revision": "d155ac2b36e289e23021",
+    "url": "js/app.202c4a57.js"
+  },
+  {
+    "revision": "5882b729780f95cf77bd",
+    "url": "js/chunk-vendors.b947ef11.js"
+  },
+  {
+    "revision": "a7a90ac83e17da969886",
+    "url": "js/xabout.7b6a72a6.js"
+  },
+  {
+    "revision": "3f70694a81735fa0c8c57e90bd4209b9",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
