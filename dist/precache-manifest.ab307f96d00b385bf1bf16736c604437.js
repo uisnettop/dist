617de3eeafcf3ad213963f26e2ba96b571424a0d
uisnettop <84020505+uisnettop@users.noreply.github.com>
2021-11-11 15:32:08 +0100

    test

--- dist/precache-manifest.ab307f96d00b385bf1bf16736c604437.js
+++ dist/precache-manifest.ab307f96d00b385bf1bf16736c604437.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8b8c70f2ab19d777f08d",
+    "revision": "71fe8da1cf249b8149a7",
     "url": "css/app.26932dd1.css"
   },
   {
@@ -12,12 +12,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "img/logo.82b9c7a5.png"
   },
   {
-    "revision": "17edcb7aa097a818502154f70a759934",
+    "revision": "45bd4b4439c04520704e62f03f6e4209",
     "url": "index.html"
   },
   {
-    "revision": "8b8c70f2ab19d777f08d",
-    "url": "js/app.c801a51b.js"
+    "revision": "71fe8da1cf249b8149a7",
+    "url": "js/app.9042c3bf.js"
   },
   {
     "revision": "c77161275fbda724fcbe",
@@ -36,7 +36,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "robots.txt"
   },
   {
-    "revision": "93158b0afe5b7de1b94f5a95099f4a66",
+    "revision": "033c5316ca8e52e8c0f20d448f2908b9",
     "url": "service-worker.js"
   }
 ]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,7 +1,10 @@
-importScripts("precache-manifest.75830588ef224876da908f9d45509593.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.ab307f96d00b385bf1bf16736c604437.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
 debugger // eslint-disable-line no-debugger
+
+const appVersionNumber="0.3.3"
+
 if (workbox) {
   console.log(`Workbox is loaded`);
   workbox.precaching.precacheAndRoute(self.__precacheManifest);
@@ -10,6 +13,29 @@ else {
   console.log(`Workbox didn't load`);
 }
 
-workbox.routing.registerRoute('https://www8.uis.no/fag/vue/externalLoads/lang.json', workbox.strategies.cacheFirst({
+workbox.routing.registerRoute('https://www8.uis.no/fag/vue/externalLoads/lang.json', 
+  workbox.strategies.networkFirst({
+    networkTimeoutSeconds: 5,
     cacheName: 'placeholder-cache',
 }));
+
+
+
+
+
+/*
+Offline Post Requests With Workbox (VueJS)
+https://medium.com/@mario.brendel1990/offline-post-requests-with-workbox-vuejs-4df0e9f93da9
+
+const bgSyncPlugin = new workbox.backgroundSync.Plugin('queueExample', {
+  maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
+});
+workbox.routing.registerRoute(
+  'https://jsonplaceholder.typicode.com/posts',
+  workbox.strategies.networkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  'POST'
+);
+
+*/
